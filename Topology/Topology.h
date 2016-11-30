192ad8f807c1b4c1bf477ec06f6103012f70130b
FredrikSjoberg <fredrik.sjoberg.swe@gmail.com>
2016-11-30 10:32:38 +0100

    Initial Commit

---
+++ Topology/Topology.h
@@ -0,0 +1,19 @@
+//
+//  Topology.h
+//  Topology
+//
+//  Created by Fredrik Sjöberg on 30/11/16.
+//  Copyright © 2016 KnightsFee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Topology.
+FOUNDATION_EXPORT double TopologyVersionNumber;
+
+//! Project version string for Topology.
+FOUNDATION_EXPORT const unsigned char TopologyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Topology/PublicHeader.h>
+
+
