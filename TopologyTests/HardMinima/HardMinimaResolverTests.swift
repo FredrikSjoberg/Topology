38234c49325a6c987b845eeba38a2f90c1f158a5
Fredrik Sjöberg <fredrik.sjoberg@oneagency.se>
2016-12-21 10:17:04 +0100

    fixed test coverage and updated throwing semantics

--- TopologyTests/HardMinima/HardMinimaResolverTests.swift
+++ TopologyTests/HardMinima/HardMinimaResolverTests.swift
@@ -17,13 +17,10 @@ class HardMinimaResolverTests: QuickSpec {
             it("should init correctly") {
                 let resolver = HardMinimaResolver<TestCorner>(corners: [])
                 expect(resolver.isEmpty).to(beTrue())
-                do {
-                    let result = try resolver.process()
-                    expect(result).to(beNil())
-                }
-                catch {
-                    print(error)
-                }
+                
+                var result: HardMinimaSolution<TestCorner>!
+                expect { result = try resolver.process() }.toNot(throwError())
+                expect(result).to(beNil())
             }
         }
         
@@ -38,64 +35,120 @@ class HardMinimaResolverTests: QuickSpec {
 //             */
 //            1.5 c3
         
-        let minima = TestCorner()
-        let c0 = TestCorner(elevation: 1)
-        let c1 = TestCorner(elevation: 1.3)
-        let c2 = TestCorner(elevation: 1.6)
-        let c3 = TestCorner(elevation: 1.5)
-        let c4 = TestCorner(elevation: 2)
-        minima.adjacent = [c0,c1,c2,c3,c4]
-        c0.adjacent = [c1,minima,c4]
-        c1.adjacent = [c2,minima,c0]
-        c2.adjacent = [c3,minima,c1]
-        c3.adjacent = [c2,minima,c4]
-        c4.adjacent = [c0,minima,c3]
         
-        let corners = [minima,c0,c1,c2,c3,c4]
+        var minima: TestCorner!
+        var c0: TestCorner!
+        var c1: TestCorner!
+        var c2: TestCorner!
+        var c3: TestCorner!
+        var c4: TestCorner!
+        var corners: [TestCorner]!
+        
+        beforeEach {
+            minima = TestCorner()
+            c0 = TestCorner(elevation: 1)
+            c1 = TestCorner(elevation: 1.3)
+            c2 = TestCorner(elevation: 1.6)
+            c3 = TestCorner(elevation: 1.5)
+            c4 = TestCorner(elevation: 2)
+            minima.adjacent = [c0,c1,c2,c3,c4]
+            c0.adjacent = [c1,minima,c4]
+            c1.adjacent = [c2,minima,c0]
+            c2.adjacent = [c3,minima,c1]
+            c3.adjacent = [c2,minima,c4]
+            c4.adjacent = [c0,minima,c3]
+            
+            corners = [minima,c0,c1,c2,c3,c4]
+        }
         
         describe("Simple Solution") {
             it("should resolve simple minima") {
                 let resolver = HardMinimaResolver(corners: corners)
                 expect(resolver.isEmpty).to(beFalse())
-                do {
-                    let result = try resolver.process()
-                    expect(result).toNot(beNil())
-                    expect(result!.elevation).to(equal(1))
-                    result!.corner.elevation = result!.elevation
-                    
-                    let next = try resolver.process()
-                    expect(next).to(beNil())
-                    expect(resolver.isEmpty).to(beTrue())
-                }
-                catch {
-                    print(error)
-                }
+                
+                var result: HardMinimaSolution<TestCorner>!
+                expect { result = try resolver.process() }.toNot(throwError())
+                expect(result).toNot(beNil())
+                expect(result!.elevation).to(equal(1))
+                result!.corner.elevation = result!.elevation
+                
+                var next: HardMinimaSolution<TestCorner>!
+                expect{ next = try resolver.process() }.toNot(throwError())
+                expect(next).to(beNil())
+                expect(resolver.isEmpty).to(beTrue())
             }
         }
         
         describe("Linked Minima") {
             it("should resolve linked minima") {
-                minima.elevation = 0
                 c4.elevation = 0
                 
                 let resolver = HardMinimaResolver(corners: corners)
                 expect(resolver.isEmpty).to(beFalse())
-                do {
-                    let result = try resolver.process()
-                    expect(result).toNot(beNil())
-                    expect(result!.elevation).to(equal(0.9))
-                    result!.corner.elevation = result!.elevation
-                    
-                    let next = try resolver.process()
-                    expect(next).toNot(beNil())
-                    expect(next!.elevation).to(equal(0.9*0.9))
-                    next!.corner.elevation = next!.elevation
-                    
-                    expect(resolver.isEmpty).to(beTrue())
-                }
-                catch {
-                    print(error)
-                }
+                
+                var result: HardMinimaSolution<TestCorner>!
+                expect { result = try resolver.process() }.toNot(throwError())
+                expect(result).toNot(beNil())
+                expect(result!.elevation).to(equal(0.9))
+                result!.corner.elevation = result!.elevation
+                
+                var next: HardMinimaSolution<TestCorner>!
+                expect{ next = try resolver.process() }.toNot(throwError())
+                expect(next).toNot(beNil())
+                expect(next!.elevation).to(equal(0.9*0.9))
+                next!.corner.elevation = next!.elevation
+                
+                expect(resolver.isEmpty).to(beTrue())
+            }
+        }
+        
+        describe("Add Corners") {
+            it("should only add corners to unprocessed list that are hard zero elevation minimas") {
+                let c5 = TestCorner()
+                let c6 = TestCorner(elevation: 1.2)
+                c2.adjacent.insert(c5)
+                c5.adjacent.insert(c2)
+                c5.adjacent.insert(c6)
+                c6.adjacent.insert(c3)
+                
+                let resolver = HardMinimaResolver(corners: corners)
+                resolver.addCorners(corners: [c5, c6])
+                
+                expect(resolver.isEmpty).to(beFalse())
+                
+                var result: HardMinimaSolution<TestCorner>!
+                expect{ result = try resolver.process() }.toNot(throwError())
+                
+                expect(result).toNot(beNil())
+                result!.corner.elevation = result!.elevation
+                expect(resolver.isEmpty).to(beFalse())
+                
+                var next: HardMinimaSolution<TestCorner>!
+                expect{ next = try resolver.process() }.toNot(throwError())
+                expect(next).toNot(beNil())
+                next!.corner.elevation = next!.elevation
+                
+                expect(resolver.isEmpty).to(beTrue())
+                
+            }
+        }
+        
+        describe("Errors") {
+            it("Should throw error if minima is ocean") {
+                minima.isOcean = true
+                let resolver = HardMinimaResolver(corners: corners)
+                
+                expect(resolver.isEmpty).to(beFalse())
+                expect{ try resolver.process() }.to(throwError(errorType: HardMinimaResolverError<TestCorner>.self))
+            }
+            
+            it("should throw error if all corners are hard minima") {
+                corners.forEach{ $0.elevation = 0 }
+                
+                let resolver = HardMinimaResolver(corners: corners)
+                
+                expect(resolver.isEmpty).to(beFalse())
+                expect{ try resolver.process() }.to(throwError(errorType: HardMinimaResolverError<TestCorner>.self))
             }
         }
     }
